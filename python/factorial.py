1e08d324bfc1e13dc7c9737a3fbccc3078b14cb4
Omprakash Kumar <omprxz@gmail.com>
2023-05-20 16:50:14 +0530

    ok

--- python/factorial.py
+++ python/factorial.py
@@ -3,10 +3,13 @@ num=input('Enter number: ')
 if(num.isdigit()==True):
     num=int(num)
     mainNum=num
-    factorial=1
-    while(num>1):
-        factorial=factorial*num
-        num-=1
+    if(num==0 or num==1):
+        factorial=1
+    else:
+        factorial=1
+        while(num>1):
+            factorial=factorial*num
+            num-=1
     print('Factorial of',mainNum,'is',factorial)
 else:
     print('!!! Only numbers are allowed !!!')
